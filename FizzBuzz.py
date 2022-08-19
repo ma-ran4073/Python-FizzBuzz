f6516884f55f7681930763fae5e60ff2f44e7eca
delver-blog <727721eucb032@skcet.ac.in>
2022-08-19 16:09:56 +0530

    fizzbuzz_update

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,12 +1,13 @@
 #the_multiples-FizzBuzz
-for n in range(1,1000):
-    if(n%3==0 and n%5==0):
-        print(str(n)+"= FizzBuzz")
-    else:
-        if(n%3==0):
-            print(str(n)+"= Fizz")
+def fizzbuzz(r):
+    for n in range(1,r):
+        if(n%3==0 and n%5==0):
+            print(str(n)+"= FizzBuzz")
         else:
-            if(n%5==0):
-                print(str(n)+"= Buzz")
+            if(n%3==0):
+                print(str(n)+"= Fizz")
             else:
-                print(n)
+                if(n%5==0):
+                    print(str(n)+"= Buzz")
+                else:
+                    print(n)
